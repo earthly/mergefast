1ae074af3ae8ca16bc0185b7dadad96b2762ec77
Adam Gordon Bell <adam@corecursive.com>
2023-11-10 09:22:23 -0500

    first attempt

--- mergefast/mergefast_rust/src/core.rs
+++ mergefast/mergefast_rust/src/core.rs
@@ -7,29 +7,29 @@ use pyo3::types::{PyList, PyAny};
 use pyo3::PyResult;
 use pyo3::Python;
 
-fn object_compare(v: &PyAny, w: &PyAny) -> PyResult<bool> {
+pub fn object_compare(v: &PyAny, w: &PyAny) -> PyResult<bool> {
     v.lt(w)
 }
 
-fn float_compare(v: &PyAny, w: &PyAny) -> PyResult<bool> {
+pub fn float_compare(v: &PyAny, w: &PyAny) -> PyResult<bool> {
     let v_float = v.downcast::<PyFloat>()?.value();
     let w_float = w.downcast::<PyFloat>()?.value();
     Ok(v_float < w_float)
 }
 
-fn long_compare(v: &PyAny, w: &PyAny) -> PyResult<bool> {
+pub fn long_compare(v: &PyAny, w: &PyAny) -> PyResult<bool> {
     let v_int = v.extract::<i64>()?;
     let w_int = w.extract::<i64>()?;
     Ok(v_int < w_int)
 }
 
-fn latin_compare(v: &PyAny, w: &PyAny) -> PyResult<bool> {
+pub fn latin_compare(v: &PyAny, w: &PyAny) -> PyResult<bool> {
     let v_str = v.downcast::<PyString>()?.to_str()?;
     let w_str = w.downcast::<PyString>()?.to_str()?;
     Ok(v_str < w_str)
 }
 
-fn merge_internal(py: Python, list1: &PyList, list2: &PyList, compare: &dyn Fn(&PyAny, &PyAny) -> PyResult<bool>) -> PyResult<Py<PyList>> {
+pub fn merge_internal(py: Python, list1: &PyList, list2: &PyList, compare: &dyn Fn(&PyAny, &PyAny) -> PyResult<bool>) -> PyResult<Py<PyList>> {
     let n1 = list1.len();
     let n2 = list2.len();
     let merged_list = PyList::empty(py);
@@ -67,21 +67,21 @@ fn merge_internal(py: Python, list1: &PyList, list2: &PyList, compare: &dyn Fn(&
 }
 
 #[pyfunction]
-fn merge(py: Python, list1: &PyList, list2: &PyList) -> PyResult<Py<PyList>> {
+pub fn merge(py: Python, list1: &PyList, list2: &PyList) -> PyResult<Py<PyList>> {
     merge_internal(py, list1, list2, &object_compare)
 }
 
 #[pyfunction]
-fn merge_latin(py: Python, list1: &PyList, list2: &PyList) -> PyResult<Py<PyList>> {
+pub fn merge_latin(py: Python, list1: &PyList, list2: &PyList) -> PyResult<Py<PyList>> {
     merge_internal(py, list1, list2, &latin_compare)
 }
 
 #[pyfunction]
-fn merge_int(py: Python, list1: &PyList, list2: &PyList) -> PyResult<Py<PyList>> {
+pub fn merge_int(py: Python, list1: &PyList, list2: &PyList) -> PyResult<Py<PyList>> {
     merge_internal(py, list1, list2, &long_compare)
 }
 
 #[pyfunction]
-fn merge_float(py: Python, list1: &PyList, list2: &PyList) -> PyResult<Py<PyList>> {
+pub fn merge_float(py: Python, list1: &PyList, list2: &PyList) -> PyResult<Py<PyList>> {
     merge_internal(py, list1, list2, &float_compare)
 }
---
+++ mergefast/mergefast_rust/src/lib.rs
@@ -0,0 +1,5 @@
+mod bind;
+mod core;
+
+pub use bind::*;
+pub use core::*;
